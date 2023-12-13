58b6819441111becc901a7bc143ab2e8d7531009
Chi Chova <chi.chova@mixpanel.com>
2023-12-13 06:25:37 -0500

    Save incomplete day 12

--- 12.py
+++ 12.py
@@ -45,22 +45,110 @@ def generate_record_possibilities(record):
         if record_possibility.count('#') == total_damaged_springs:
             yield Record(record_possibility, record.damaged_groups)
 
-def unfold_record(record):
+def unfold_record(record, factor):
     springs, damaged_groups = record
-    unfolded_springs = '?'.join([springs] * 5)
-    unfolded_groups = damaged_groups * 5
+    unfolded_springs = '?'.join([springs] * factor)
+    unfolded_groups = damaged_groups * factor
     return Record(unfolded_springs, unfolded_groups)
+
+def count_record_possibilities(record):
+    total_possibilities = 0
+    start_damaged = 0
+    end_damaged = 0
+    for possibility in generate_record_possibilities(record):
+        if is_valid_record(possibility):
+            total_possibilities += 1
+            if possibility.springs[0] == '#':
+                start_damaged += 1
+            if possibility.springs[-1] == '#':
+                end_damaged += 1
+    return total_possibilities, start_damaged, end_damaged
 
 def count_valid_arrangements(records, unfold=False):
     valid_arrangements = 0
     for i, record in enumerate(records):
+        possibilities, start_damaged, end_damaged = count_record_possibilities(record)
+
         if unfold:
-            record = unfold_record(record)
-        for possibility in generate_record_possibilities(record):
-            if is_valid_record(possibility):
-                valid_arrangements += 1
-        # print(f'Record {i}: {valid_arrangements} cumulative valid arrangements')
+            # This approach tries to extrapolate the sum from adding a single '?'
+            # It works on the sample input but will undercount certain input
+            springs, damaged_groups = record
+            start_undamaged = possibilities - start_damaged
+            end_undamaged = possibilities - end_damaged
+
+            left_unfold = Record(f'{springs}?', damaged_groups)
+            left_possibilities, left_start_damaged, left_end_damaged = count_record_possibilities(left_unfold)
+            left_end_undamaged = left_possibilities - left_end_damaged
+
+            right_unfold = Record(f'?{springs}', damaged_groups)
+            right_possibilities, right_start_damaged, right_end_damaged = count_record_possibilities(right_unfold)
+            right_start_undamaged = right_possibilities - right_start_damaged
+
+            unfolded_possibilities = max(
+                start_undamaged * left_possibilities + start_damaged * left_end_undamaged,
+                end_undamaged * right_possibilities + end_damaged * right_start_undamaged,
+            )
+            possibilities = possibilities * (unfolded_possibilities // possibilities) ** 4
+
+            """
+            This is another approach that tries to extrapolate the 5x unfold from 2x.
+            It's still way too slow coupled with brute forcing the possibilites. And I'm
+            not certain it would be correct for certain inputs.
+
+            double_possibilities, _, _ = count_record_possibilities(unfold_record(record, 2))
+            possibilities = possibilities * (double_possibilities // possibilities) ** 4
+
+            Record 0: 2592 valid arrangements
+            Record 1: 1259712 valid arrangements
+            Record 2: 16 valid arrangements
+            Record 3: 60000 valid arrangements
+            Record 4: 1875 valid arrangements
+            Record 5: 65610000 valid arrangements
+            Record 6: 32 valid arrangements
+            Record 7: 5308416 valid arrangements
+            Record 8: 162 valid arrangements
+            Record 9: 19683 valid arrangements
+            Record 10: 1250 valid arrangements
+            Record 11: 18708704 valid arrangements
+            Record 12: 768 valid arrangements
+            Record 13: 124416 valid arrangements
+            Record 14: 24576 valid arrangements
+            Record 15: 5859375 valid arrangements
+            Record 16: 1250 valid arrangements
+            Record 17: 56224830 valid arrangements
+            """
+
+
+        print(f'Record {i}: {possibilities} valid arrangements')
+        valid_arrangements += possibilities
     return valid_arrangements
 
 print('Part 1: ', count_valid_arrangements(records, unfold=False)) # 7118
-print('Part 2: ', count_valid_arrangements(records, unfold=True)) #
+
+sample_records = [                          #                        L  R    x2
+    Record('?.??#?', [1, 2]),               #     12 from 3       -  5, 4    12    12 // 3 ==  4
+    Record('???.###', [1,1,3]),             #      1 from 1       -  1, 1     1    1 //  1 ==  1
+    Record('.??..??...?##.', [1,1,3]),      #  16384 from 4       -  8, 4    32   32 //  4 ==  8
+    Record('?#?#?#?#?#?#?#?', [1,3,1,6]),   #      1 from 1       -  1, 1     1    1 //  1 ==  1
+    Record('????.#...#...', [4,1,1]),       #     16 from 1       -  2, 1     2    2 //  1 ==  2
+    Record('????.######..#####.', [1,6,5]), #   2500 from 4       -  5, 4    20   20 //  4 ==  5
+    Record('?###????????', [3,2,1]),        # 506250 from 10      - 10, 15  150  150 // 10 == 15
+]
+
+""" unfold left              unfold right
+       ?.??#??     ?.??#?   ??.??#?            ?.??#???.??#?
+                                                   # ## # ##  <-- How to catch this case??
+       #..##..     #..##.   #...##.            #..##.#...##.      Not possible with current approach
+                                               #..##.#....##
+       #...##.     #...##   .#..##.            #..##..#..##.
+       ..#.##.     ..#.##   #....##            #..##..#...##
+                            .#...##            #..##....#.##
+                            ...#.##            #...##.#..##.
+                                               #...##.#...##
+                                               #...##...#.##
+                                               ..#.##.#..##.
+                                               ..#.##.#...##
+                                               ..#.##...#.##
+"""
+
+print('Part 2: ', count_valid_arrangements(sample_records, unfold=True)) # 316598322037 is too low
