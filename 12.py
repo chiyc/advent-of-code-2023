b739a051868608acd35314aad3a75116ce57f622
Chi Chova <chi.chova@mixpanel.com>
2023-12-14 09:47:08 -0500

    Clean up and comment day 12

--- 12.py
+++ 12.py
@@ -25,9 +25,12 @@ def find_next_match(remaining_springs, damaged_group):
 
     group_span = group_match.span(1)
     if '#' in remaining_springs[0:group_span[0]]:
+        # The current damaged group must not skip any damaged springs
         return None
 
     return group_span
+
+# Testing that the find_next_match regex works as expected
 
 assert find_next_match('###', 3) == (0, 3)
 assert find_next_match('???', 3) == (0, 3)
@@ -56,13 +59,17 @@ def count_record_possibilities(record):
 
     remaining_groups = damaged_groups.copy()
     if len(remaining_groups) == 0 and '#' in remaining_springs:
+        # Failure base case, no damaged groups left for damaged spring leftover
         return 0
-    elif len(remaining_groups) == 0:
+
+    if len(remaining_groups) == 0:
+        # Successful base case, matched all groups
         return 1
 
     current_group = remaining_groups.pop(0)
     group_match = find_next_match(remaining_springs, current_group)
     if group_match is None:
+        # Failed to match the group against the remaining springs
         return 0
 
     possibilities = 0
@@ -70,41 +77,38 @@ def count_record_possibilities(record):
     while more_matches:
         next_remaining_start = group_match[1] + 1
         next_springs = remaining_springs[next_remaining_start:]
+
+        # For each possible arrangement of the current group, we multiply it with
+        # the possible arrangements of the remaining groups.
+        #
+        # A result of 0 indicates an impossible arrangement, and this will propagate
+        # recursively back to the first group, invalidating the arrangement.
         possibilities += 1 * count_record_possibilities(Record(next_springs, remaining_groups))
 
         if remaining_springs[group_match[0]] == '#':
+            # No more other positions are possible for the current group since we can't
+            # skip damaged springs in the record
             more_matches = False
         else:
             remaining_springs = remaining_springs[group_match[0]+1:]
             group_match = find_next_match(remaining_springs, current_group)
             if group_match is None:
                 more_matches = False
+
     memo[memo_record] = possibilities
     return possibilities
 
 def count_valid_arrangements(records, unfold=False):
     valid_arrangements = 0
-    for i, record in enumerate(records):
+    for record in records:
         possibilities = count_record_possibilities(record)
-        # print(f'Record {i}: {record.springs} - {record.damaged_groups} - {possibilities} arrangements')
         if unfold:
-            possibilities = count_record_possibilities(unfold_record(record, 1))
             possibilities = count_record_possibilities(unfold_record(record, 2))
             possibilities = count_record_possibilities(unfold_record(record, 3))
             possibilities = count_record_possibilities(unfold_record(record, 4))
             possibilities = count_record_possibilities(unfold_record(record, 5))
         valid_arrangements += possibilities
     return valid_arrangements
-
-sample_records = [                          #                        L  R    x2
-    # Record('?.??#?', [1, 2]),               #     12 from 3       -  5, 4    12    12 // 3 ==  4    new approach 5
-    Record('???.###', [1,1,3]),             #      1 from 1       -  1, 1     1    1 //  1 ==  1    new approach 1
-    Record('.??..??...?##.', [1,1,3]),      #  16384 from 4       -  8, 4    32   32 //  4 ==  8    new approach 41
-    Record('?#?#?#?#?#?#?#?', [1,3,1,6]),   #      1 from 1       -  1, 1     1    1 //  1 ==  1    new approach 1
-    Record('????.#...#...', [4,1,1]),       #     16 from 1       -  2, 1     2    2 //  1 ==  2    new approach 1
-    Record('????.######..#####.', [1,6,5]), #   2500 from 4       -  5, 4    20   20 //  4 ==  5    new approach 4
-    Record('?###????????', [3,2,1]),        # 506250 from 10      - 10, 15  150  150 // 10 == 15    new approach 10
-] # 24 total for part 1
 
 print('Part 1: ', count_valid_arrangements(records, unfold=False)) # 7118
 print('Part 2: ', count_valid_arrangements(records, unfold=True))  # 7030194981795
