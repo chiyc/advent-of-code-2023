80518498521799112b042110a9c6e312d37c3587
Chi Chova <chi.chova@mixpanel.com>
2023-12-23 10:57:36 -0500

    Finish day 23 part 2

--- 23.py
+++ 23.py
@@ -9,17 +9,23 @@ for row in read_input(23):
 Pos = namedtuple('Pos', ['i', 'j'])
 Path = namedtuple('Path', ['head', 'steps', 'prev'])
 
-def longest_path_to_end(grid, icy_slopes=True):
-    rows = len(grid)
-    cols = len(grid[0])
-    end_i = rows - 1
+def find_start(grid):
+    start_i = 0
+    start_j = grid[start_i].index('.')
+    return Pos(start_i, start_j)
+
+def find_end(grid):
+    end_i = len(grid) - 1
     end_j = grid[end_i].index('.')
+    return Pos(end_i, end_j)
+
+def find_longest_path_with_icy_slopes(grid):
+    rows = len(grid)
+    end_i, end_j = find_end(grid)
     end_pos = Pos(end_i, end_j)
 
     max_steps = {}
-
-    start_i = 0
-    start_j = grid[start_i].index('.')
+    start_i, start_j = find_start(grid)
     next = deque([Path(Pos(start_i, start_j), 0, [])])
     while next:
         path = next.popleft()
@@ -43,16 +49,16 @@ def longest_path_to_end(grid, icy_slopes=True):
 
         next_steps = len(path.prev) + 1
         next_prev = path.prev + [path.head]
-        if icy_slopes and grid[i][j] == '^':
+        if grid[i][j] == '^':
             next.append(Path(Pos(i - 1, j), next_steps, next_prev))
 
-        elif icy_slopes and grid[i][j] == 'v':
+        elif grid[i][j] == 'v':
             next.append(Path(Pos(i + 1, j), next_steps, next_prev))
 
-        elif icy_slopes and grid[i][j] == '<':
+        elif grid[i][j] == '<':
             next.append(Path(Pos(i, j - 1), next_steps, next_prev))
 
-        elif icy_slopes and grid[i][j] == '>':
+        elif grid[i][j] == '>':
             next.append(Path(Pos(i, j + 1), next_steps, next_prev))
 
         else:
@@ -65,5 +71,102 @@ def longest_path_to_end(grid, icy_slopes=True):
 
     return max_steps[end_pos]
 
-print('Part 1: ', longest_path_to_end(grid)) # 2326
-print('Part 2: ', longest_path_to_end(grid, icy_slopes = False)) # too slow
+print('Part 1: ', find_longest_path_with_icy_slopes(grid)) # 2326
+# print('Part 2: ', find_longest_path(grid, icy_slopes = False)) # too slow
+
+Edge = namedtuple('Edge', ['head', 'weight'])
+
+def find_intersections(grid):
+    intersections = []
+    for i in range(1, len(grid) - 1):
+        for j in range(1, len(grid[0]) - 1):
+            crossroads = [
+                grid[i - 1][j],
+                grid[i + 1][j],
+                grid[i][j - 1],
+                grid[i][j + 1],
+            ]
+
+            if (
+                grid[i][j] in ('.', '^', 'v', '<', '>')
+                and len(list(filter(lambda tile: tile in ('.', '^', 'v', '<', '>'), crossroads))) > 2
+            ):
+                intersections.append(Pos(i, j))
+    return intersections
+
+def build_node_edges(starting_node, grid, nodes):
+    edges = []
+    next = deque([Path(starting_node, 0, [])])
+    while next:
+        path = next.popleft()
+        i, j = path.head
+
+        if i < 0 or i >= len(grid):
+            continue
+
+        if grid[i][j] == '#':
+            continue
+
+        if path.head in path.prev:
+            continue
+
+        if path.head != starting_node and path.head in nodes:
+            edges.append(Edge(path.head, path.steps))
+            continue
+
+        next_steps = path.steps + 1
+        next_prev = path.prev + [path.head]
+        next.extend([
+            Path(Pos(i - 1, j), next_steps, next_prev),
+            Path(Pos(i + 1, j), next_steps, next_prev),
+            Path(Pos(i, j - 1), next_steps, next_prev),
+            Path(Pos(i, j + 1), next_steps, next_prev),
+        ])
+    return edges
+
+def build_weighted_graph(grid):
+    graph = {}
+
+    intersections = find_intersections(grid)
+    nodes = intersections + [find_start(grid), find_end(grid)]
+    for node in nodes:
+        if node not in graph:
+            graph[node] = []
+        edges = build_node_edges(node, grid, nodes)
+        graph[node] = edges
+        for edge in edges:
+            if edge not in graph[node]:
+                graph[node].append(edge)
+            other_node = edge.head
+            if other_node not in graph:
+                graph[other_node] = []
+            if Edge(node, edge.weight) not in graph[other_node]:
+                graph[other_node].append(Edge(node, edge.weight))
+
+    return graph
+
+def find_longest_path_on_graph(graph, start, end):
+    max_steps = {}
+    next = deque([Path(start, 0, [])])
+    while next:
+        path = next.popleft()
+
+        if path.head in path.prev:
+            continue
+
+        if path.head == end:
+            if end not in max_steps:
+                max_steps[end] = path.steps
+            elif path.steps > max_steps[end]:
+                max_steps[end] = path.steps
+            continue
+
+        next_prev = path.prev + [path.head]
+        next.extend([
+            Path(edge.head, path.steps + edge.weight, next_prev)
+            for edge in graph[path.head]
+        ])
+    return max_steps[end]
+
+graph = build_weighted_graph(grid)
+print('Part 2: ', find_longest_path_on_graph(graph, find_start(grid), find_end(grid))) # 6574
